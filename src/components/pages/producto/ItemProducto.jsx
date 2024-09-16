64250e3f5ef66a155fb84d9dc51546025a3f89d6
Gabriel Haurane <gabrielhaurane@gmail.com>
2024-09-16 11:19:58 -0300

    actualizamos la tabla de productos luego de eliminar

--- src/components/pages/producto/ItemProducto.jsx
+++ src/components/pages/producto/ItemProducto.jsx
@@ -1,7 +1,7 @@
 import { Button } from "react-bootstrap";
 import Swal from "sweetalert2";
-import { borrarProductoAPI } from "../../../helpers/queries";
-const ItemProducto = ({ producto, fila }) => {
+import { borrarProductoAPI, leerProductosAPI } from "../../../helpers/queries";
+const ItemProducto = ({ producto, fila, setListaProductos }) => {
   const borrarProducto = () => {
     Swal.fire({
       title: "estas seguro de borrar el producto?",
@@ -22,6 +22,14 @@ const ItemProducto = ({ producto, fila }) => {
             text: `el producto ${producto.nombreProducto}, fue borrado correctamente`,
             icon: "success"
           });
+          // actualizar el state del administrador
+          const productoAPI = await leerProductosAPI();
+          if (productoAPI.status === 200) {
+            const productosActualizados = await productoAPI.json()
+            setListaProductos(productosActualizados)
+          } 
+          // setListaProductos
+
         } else {
           Swal.fire({
             title: "Ocurrio un error",
